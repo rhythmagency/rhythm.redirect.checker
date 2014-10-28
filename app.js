8908a3a6bbe2baefd9dce4731ab8701e7bc64a97
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-10-27 17:02:42 -0700

    cleanup code a little

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var wait = require('wait.for');
+var request = require('request');
+var fs = require('fs-extra');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+
 if(process.argc < 4){
     console.log('usage: node app.js redirects.csv https://domain.com');
     process.exit();
@@ -7,19 +13,14 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var csvFilename = process.argv[2];
 var domainName = process.argv[3];
-
 var csvOutputFilename = 'out_' + csvFilename.split('.csv').join('.txt');
-
-var wait = require('wait.for');
-var request = require('request');
-var fs = require('fs-extra');
-var parse = require('csv-parse');
-var transform = require('stream-transform');
 
 var input = fs.createReadStream(csvFilename);
 var parser = parse({delimiter: ','});
 
+//clear output file
 fs.truncateSync(csvOutputFilename, 0);
+
 var output = fs.createWriteStream(csvOutputFilename);
 
 var transformer = transform(function(record, callback) {
@@ -29,9 +30,6 @@ var transformer = transform(function(record, callback) {
         var urlB = record[1].replace(/^https?:\/\/[^\/]+\//i, '/').replace(/^smartstopselfstorage\.com/i, '').replace('//', '/');
         if (urlB == '')
             urlB = '/';
-
-//        urlA = domainName + urlA;
-//        urlB = domainName + urlB;
 
         var line = [urlA, urlB].join('\t') + '\n';
 
