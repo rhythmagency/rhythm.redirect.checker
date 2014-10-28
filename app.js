db88fbd856e4824fc00b033e3352ffd295e4c565
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-10-28 10:21:09 -0700

    Add status code to output list as well as the actual redirected url.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var request = require('request');
 var fs = require('fs-extra');
 var parse = require('csv-parse');
 var transform = require('stream-transform');
+var url = require('url');
+var trim = require('trimmer');
 
 if(process.argc < 5){
     console.log('usage: node app.js redirects.csv domainToStrip.com https://baseURL.com');
@@ -25,23 +27,38 @@ fs.truncateSync(csvOutputFilename, 0);
 
 var output = fs.createWriteStream(csvOutputFilename);
 
+var domainRegex = new RegExp('^'+domainToStrip, 'i');
+var httpRegex = new RegExp('^https?:\/\/[^\/]+\/', 'i');
+var langPrefixRegex = new RegExp('^(/en|/es|/fr-ca|/ru)?');
+
 var transformer = transform(function(record, callback) {
     wait.launchFiber(function() {
         //console.log(record);
-        var urlA = record[0].replace(/^https?:\/\/[^\/]+\//i, '/');
-
-        var regex = new RegExp('^'+domainToStrip, 'i');
-        var urlB = record[1].replace(/^https?:\/\/[^\/]+\//i, '/').replace(regex, '').replace('//', '/');
+        var urlA = record[0].replace(httpRegex, '/').replace(langPrefixRegex, '');
+        var urlB = record[1].replace(httpRegex, '/').replace(domainRegex, '').replace('//', '/').replace(langPrefixRegex, '');
         if (urlB == '')
             urlB = '/';
 
-        var line = [urlA, urlB].join('\t') + '\n';
+        var line = [urlA, urlB].join('\t');
 
         try {
             var response = wait.forMethod(request, "head", baseURL + urlA, {followRedirect: false});
+
+            console.log(baseURL + urlA);
             console.log(response.statusCode);
-            if(response.statusCode >= 400) {
-                callback(null, line);
+
+            line = response.statusCode+'\t'+line;
+
+            var compareURLs = false;
+            var trimmedLocation = '';
+            if(response.statusCode >= 300 && response.statusCode < 400){
+                compareURLs = true;
+                trimmedLocation = trim.right(response.headers.location.replace(httpRegex, '/').replace(domainRegex, '').replace('//', '/').replace(langPrefixRegex, ''), '/');
+                line = line + '\t' + response.headers.location;
+            }
+
+            if(response.statusCode >= 400 || (compareURLs && trim.right(urlB, '/') != trimmedLocation)) {
+                callback(null, line + '\n');
             }else{
                 callback(null, '');
             }
